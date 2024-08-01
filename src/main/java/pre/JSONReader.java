45fd05fd8d4618f76c5968f453b6f3cfde3b7a91
ChenPanXYZ <me@chenpan.xyz>
2024-07-31 22:52:06 -0400

    fixed wrong class name

--- src/main/java/pre/JSONReader.java
+++ src/main/java/pre/JSONReader.java
@@ -1,41 +1,61 @@
 package pre;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class JSONObjectReader {
+public class JSONReader {
 
-    public JSONObject jsonObject;
-    private JSONObjectReader() {
+    public JSONArray jsonObject;
+    public JSONReader() {
         try {
             // Read the JSON file as a string.
-            // Note that it will be the same format as many API responses.
+            // Question: Is this an array or an object?
             String jsonString = new String(Files.readAllBytes(Paths.get("./sample.json")));
-            this.jsonObject = new JSONObject(jsonString);
+            this.jsonObject = new JSONArray(jsonString);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static JSONObjectReader getCanadaFrenchTranslation() {
-        JSONObjectReader jsonObjectReader = new JSONObjectReader();
-        // this is alreaddy an array.
-        // get the 30th element of the array
+    public String getCanadaCountryNameSpanishTranslation() {
+        // get the 30th element in the array.
+        JSONArray jsonArray = this.jsonObject;
 
-        JSONObject canadaFrenchTranslation = jsonObjectReader.jsonObject.getJSONArray("translations").getJSONObject(29);
+
+        // Note that an element of this array is a JSONObject, which is in the same format as MANY API responses.
+        JSONObject canada = jsonArray.getJSONObject(30);
+        String nameInFrench = canada.getString("es");
+        return nameInFrench;
     }
-    public static void main(String[] args) {
-        JSONObjectReader jsonObjectReader = new JSONObjectReader();
-        // Reading the data from JSON object
-        String name = jsonObjectReader.jsonObject.getString("name");
-        int age = jsonObjectReader.jsonObject.getInt("age");
-        String city = jsonObjectReader.jsonObject.getString("city");
 
-        // Print the values
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("City: " + city);
+    /**
+     * JSONReader Exercise.
+     * @param countryName the name of the country, in two letters.
+     * @param language the language to translate to, in two letters.
+     */
+    public String getCountryNameTranslation(String countryName, String language) {
+        JSONArray jsonArray = this.jsonObject;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject country = jsonArray.getJSONObject(i);
+            if (country.getString("alpha2").equals(countryName)) {
+                return country.getString(language);
+            }
+        }
+        return "Country not found";
+    }
+
+    public static void main(String[] args) {
+        JSONReader jsonReader = new JSONReader();
+
+        jsonReader.getCanadaCountryNameSpanishTranslation();
+        String translation = jsonReader.getCountryNameTranslation("ca", "es");
+        System.out.println(translation);
     }
 }
